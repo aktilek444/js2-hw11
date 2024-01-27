704da80d9f6a6ec88fd9ca5fef6f464331bff6ac
aktilek444 <150013534+aktilek444@users.noreply.github.com>
2024-01-27 17:20:12 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const numbers = [100, 55, 15, 300, 20];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+    console.log(`Сумма ${numbers[i]} + ${sum}`);
+}
+
+
+
+
